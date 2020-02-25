b26c6c6c6ed270947408d1f4ba15a940d65acdbd
MinhDat <haminhdat01@gmail.com>
2020-02-25 09:54:48 +0700

    edited notice page

--- lib/screen/Notice/main.dart
+++ lib/screen/Notice/main.dart
@@ -1,5 +1,5 @@
 import 'package:ChStore/utility/main.dart';
-import 'package:ChStore/widget/Card/CardContainer.dart';
+import 'package:ChStore/widget/main.dart';
 import 'package:flutter/material.dart';
 
 import 'NoticeItem.dart';
@@ -10,29 +10,48 @@ class Notice extends StatefulWidget {
 }
 
 class _NoticeState extends State<Notice> {
+  final ScrollController _scrollController = ScrollController();
+  bool _showHeader = NOT_SHOW_HEADER;
+  final List<Widget> _widgets = List<Widget>.generate(9, (i) => NoticeItem());
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _scrollController.addListener(() {
+      setState(() => _showHeader = _scrollController.offset > INITIAL_OFFSET);
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    // TODO: implement dispose
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final List<Widget> _widgets = List<Widget>.generate(9, (i) => NoticeItem());
-
-    return Scaffold(
-        body: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage("lib/asset/image/background/bg1_body.jpg"),
-                  fit: BoxFit.fitWidth,
-                  alignment: Alignment.topCenter),
-            ),
-            child: ListView(
-              children: <Widget>[
-                CardContainer(
-                    label: Text("Notification", style: ChTextStyle.title),
-                    color: ChColor.border,
-                    child: Container(
-                      child: Column(
-                        children: _widgets,
-                      ),
-                    ))
-              ],
-            )));
+    return ScrollableView(
+      floatingAppBar: FloatingAppBar(
+        showHeader: _showHeader,
+        header: Text("Notification", style: ChTextStyle.scrollHeader),
+        identify: Container(
+          alignment: Alignment.topLeft,
+          padding: EdgeInsets.only(left: 10.0, top: 20),
+          child: Text("Notification", style: ChTextStyle.logo),
+        ),
+      ),
+      child: ListView(controller: _scrollController, children: [
+        Container(
+          margin: EdgeInsets.only(top: 20),
+          color: ChColor.foreground.withOpacity(0.1),
+          child: CardContainer(
+            color: ChColor.border,
+            child: Container(child: Column(children: _widgets)),
+          ),
+        ),
+      ]),
+    );
   }
 }
